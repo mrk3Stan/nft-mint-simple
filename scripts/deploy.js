eaf3f52b0328d25e209066448b60dbb5a8a3e357
Mark Tristan Bustillo <markbustillo@cryptoblk.io>
2022-03-17 01:34:02 +0800

    add js files

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('EpicNFT');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to: ", nftContract.address);
+
+    // call the function
+    let txn = await nftContract.makeEpicNFT();
+    await txn.wait();
+    console.log("Minted EpicNFT #1");
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
